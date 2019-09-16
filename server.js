c6cd07e0e5c5b20d55e7397521c59cf1c7d9295b
Sangsaka Wira <sangsakawira@gmail.com>
2019-09-16 21:07:28 +0700

    sensor realtime added

--- server.js
+++ server.js
@@ -15,9 +15,14 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/set/:suhu/:humidity",(req,res)=>{
-     request.get("hfloryiot.xyz/upload2.php?id='AAAA'&jenis=1&tgl=16&bln=9&thn=2019&jam=20&mnt=34&dtk=03&t="+req.params.suhu+"&rh="+req.params.humidity+"&t1="+req.params.suhu+"&t2="+req.params.humidity+"&t3=29.8&rh1=30&rh2=70&rh3=44.2&tes=0")
-  res.sendFile(__dirname + '/index.html');
+app.get("/set/:suhu/:humidity",(req,res,next)=>{
+  request.get("http://hfloryiot.xyz/upload2.php?id='AAAA'&jenis=1&tgl=16&bln=9&thn=2019&jam=20&mnt=34&dtk=03&t="+String(req.params.suhu)+"&rh="+String(req.params.humidity)+"&t1=90&t2=70&t3=29.8&rh1=30&rh2=70&rh3=44.2&tes=0",function(err,res,body){
+    console.log("Run")
+  })
+  res.send({
+    suhu:req.params.suhu,
+    humidity:req.params.humidity
+  })
 })
 
 io.on('connection', function (socket) {
@@ -30,7 +35,7 @@ io.on('connection', function (socket) {
     console.log("humidity: "+humidity)
   });
     socket.emit('iot_push', { suhu: suhu, humidity: humidity});
-  }, 2000);
+  }, 1000);
   socket.on('iot_push', function (data) {
     console.log(data);
   });
