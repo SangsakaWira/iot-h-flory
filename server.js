fc96ab174789d2af8c8af1e3ed65a3b5ac62a0f4
Sangsaka Wira Utama <sangsakawira@Sangsakas-MacBook-Pro-3.local>
2019-10-01 07:59:41 +0700

    Server is updated independent

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let request=require('request');
 let suhu = ""
 let humidity = ""
 
-server.listen(6500,()=>{
+server.listen(7500,()=>{
     console.log("Server is running!")
 });
 
@@ -14,10 +14,19 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/set/:suhu/:humidity",(req,res,next)=>{
-  request.get("http://hfloryiot.xyz/upload2.php?id='AAAA'&jenis=1&tgl=16&bln=9&thn=2019&jam=20&mnt=34&dtk=03&t="+String(req.params.suhu)+"&rh="+String(req.params.humidity)+"&t1=90&t2=70&t3=29.8&rh1=30&rh2=70&rh3=44.2&tes=0",function(err,res,body){
-    console.log("Run")
+app.get('/getData', function (req, res) {
+  res.send({
+    suhu:suhu,
+    humidity:humidity
   })
+});
+
+app.get("/set/:suhu/:humidity",(req,res,next)=>{
+  // request.get("http://hfloryiot.xyz/upload2.php?id='AAAA'&jenis=1&tgl=16&bln=9&thn=2019&jam=20&mnt=34&dtk=03&t="+String(req.params.suhu)+"&rh="+String(req.params.humidity)+"&t1=90&t2=70&t3=29.8&rh1=30&rh2=70&rh3=44.2&tes=0",function(err,res,body){
+  //   console.log("Run")
+  // })
+  suhu=req.params.suhu
+  humidity=req.params.humidity
   res.send({
     suhu:req.params.suhu,
     humidity:req.params.humidity
@@ -26,10 +35,8 @@ app.get("/set/:suhu/:humidity",(req,res,next)=>{
 
 io.on('connection', function (socket) {
   setInterval(function(){
-    request.get('http://hfloryiot.xyz/sensor2.php?id=AAAA&jenis=1',function(err,res,body){
-    console.log(body.split("|")) 
-    suhu = body.split("|")[8]
-    humidity = body.split("|")[9]
+    request.get('http://167.71.214.196:7500/',function(err,res,body){
+    console.log(body) 
     console.log("suhu: "+suhu)
     console.log("humidity: "+humidity)
   });
